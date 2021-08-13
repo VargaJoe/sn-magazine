feaf9327595b10b6f77dc795f5c0908d1914c63a
vargajoe <varga.yosh@gmail.com>
2021-08-13 13:33:19 +0200

    example components

--- src/components/content/component-static.js
+++ src/components/content/component-static.js
@@ -1,16 +1,16 @@
 export function StaticComponent() {
   return (
-    <div className="w3-col m9">
-      <div className="w3-row-padding">
-        <div className="w3-col m12">
-          <div className="w3-card w3-round w3-white">
-            <div className="w3-container w3-padding">
-              <p>statik</p>
-            </div>
+    <div className="w3-col m9  w3-right">
+    <div className="w3-row-padding w3-margin-bottom">
+      <div className="w3-col m12">
+        <div className="w3-card w3-round w3-white">
+          <div className="w3-container w3-padding">
+            <h1>statik</h1>
           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 }
 
---
+++ src/components/content/content-link.js
@@ -0,0 +1,26 @@
+export function FolderContent(props) {
+  console.log('folder component');
+  console.log(props.data);
+  const currentPage = props.page?props.page.filter(pcnt => pcnt.Type === 'PageContainer')[0]:{};
+  return (
+    <div className="w3-col m9">
+      <div className="w3-row-padding w3-margin-bottom">
+        <div className="w3-col m12">
+          <div className="w3-card w3-round w3-white">
+            <div className="w3-container w3-padding">
+              <h1>{props.data.DisplayName}</h1>
+              <div className="context-info">
+                  <ul>
+                    <li>Component: <span>link</span></li>
+                    <li>link: <span>{props.data.Url}</span></li>
+                  </ul>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FolderContent
