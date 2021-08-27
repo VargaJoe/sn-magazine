86a0bd182686c36fc1ddd20cf5e95abb8e0bc200
vargajoe <varga.yosh@gmail.com>
2021-08-27 21:50:53 +0200

    clean compos

--- src/navigation.js
+++ src/navigation.js
@@ -1,11 +1,6 @@
 // import Home from './Home';
 import { ContentIntro } from './components/content-intro';
-// import { CategoryWrapper } from './components/category-wrapper';
-// import { ContentWrapper } from './components/content-wrapper';
-// import VanillaPageTemplate from "./components/page-templates/page-vanilla";
 import { PageWrapper } from './components/page-wrapper';
-// import Category from './Category';
-// import Translateds from './Translateds';
 import { Missing } from './components/missing';
 
 const Data = {
@@ -14,11 +9,6 @@ const Data = {
 		// 	exact: true,
 		// 	path: '/',
 		// 	component: ContentIntro
-		// },
-		// {
-		// 	exact: true,
-		// 	path: '/*',
-		// 	component: CategoryWrapper
 		// },
 		{
 			exact: true,
