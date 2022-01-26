48216ac36f5eb4418acbf167177691cfb5b1a864
vargajoe <varga.yosh@gmail.com>
2022-01-26 20:30:35 +0100

    custom project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import "./App.css";
 import { SideMenu } from "./components/side-menu";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SiteRoutes from "./navigation";
+import { Link } from 'react-router-dom';
+const DATA = require('./config.json');
+const defaultImage = require('./images/logo.png');
 
 function App() {
+  let logoPath = process.env.REACT_APP_LOGO_PATH || DATA.siteLogo;
+  let apiUrl = DATA.apiUrl;
+  let logoUrl = apiUrl + logoPath;
+		if (logoPath === undefined || logoUrl === apiUrl) {
+			logoUrl = defaultImage;
+		}
+
   return (
+    <BrowserRouter>
     <div className="App w3-theme-l5">
       <div className="w3-top">
         <div className="w3-bar w3-theme-d2 w3-left-align w3-large">
@@ -14,12 +25,16 @@ function App() {
           >
             <i className="fa fa-bars"></i>
           </a>
-          <a
+          <Link to={'/'}>
+            <img src={logoUrl} height='52px' alt="site title" className="w3-bar-item w3-button w3-padding-large w3-theme-d4" />
+          </Link> 
+          {/* <a
             href="/"
-            className="w3-bar-item w3-button w3-padding-large w3-theme-d4"
+            className="w3-bar-item w3-button w3-padding-large w3-theme-d4"            
           >
             <i className="fa fa-home w3-margin-right"></i>Logo
-          </a>
+            
+          </a> */}
           <a
             href="/"
             className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"
@@ -30,47 +45,9 @@ function App() {
           <a
             href="/"
             className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"
-            title="Account Settings"
-          >
-            <i className="fa fa-user"></i>
-          </a>
-          <a
-            href="/"
-            className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"
-            title="Messages"
+            title="Send Message"
           >
             <i className="fa fa-envelope"></i>
-          </a>
-          <div className="w3-dropdown-hover w3-hide-small">
-            <button
-              className="w3-button w3-padding-large"
-              title="Notifications"
-            >
-              <i className="fa fa-bell"></i>
-              <span className="w3-badge w3-right w3-small w3-green">3</span>
-            </button>
-            <div className="w3-dropdown-content w3-card-4 w3-bar-block w3-dropdown-content-custom">
-              <a href="/" className="w3-bar-item w3-button">
-                One new friend request
-              </a>
-              <a href="/" className="w3-bar-item w3-button">
-                John Doe posted on your wall
-              </a>
-              <a href="/" className="w3-bar-item w3-button">
-                Jane likes your post
-              </a>
-            </div>
-          </div>
-          <a
-            href="/"
-            className="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white"
-            title="My Account"
-          >
-            <img
-              src="/w3images/avatar2.png"
-              className="w3-circle w2-circle-custom"
-              alt="Avatar"
-            />
           </a>
         </div>
       </div>
@@ -84,7 +61,7 @@ function App() {
   <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 3</a>
   <a href="#" className="w3-bar-item w3-button w3-padding-large">My Profile</a> */}
       </div>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
       {/* Page Container */}
       <div className="w3-container w3-content w3-content-custom">        
         {/* The Grid */}
@@ -121,7 +98,7 @@ function App() {
         </div>
         {/* End Grid */}
       </div>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
       {/* End Page Container */}
 
       {/* Footer */}
@@ -139,6 +116,7 @@ function App() {
       </footer>
       {/* End Footer */}
     </div>
+    </BrowserRouter>
   );
 }
 
