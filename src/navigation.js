078c2dabc86935310a33792c0603d472a02bd67f
vargajoe <varga.yosh@gmail.com>
2022-02-12 00:36:18 +0100

    cleanup

--- src/navigation.js
+++ src/navigation.js
@@ -1,15 +1,8 @@
-// import Home from './Home';
-import { ContentIntro } from './components/content-intro';
 import { PageWrapper } from './components/page-wrapper';
 import { Missing } from './components/missing';
 
 const Data = {
 	public: [
-		// {
-		// 	exact: true,
-		// 	path: '/',
-		// 	component: ContentIntro
-		// },
 		{
 			exact: true,
 			path: '/*',
