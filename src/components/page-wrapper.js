23538f83d381c222e385f040c5df8edc3aee9549
vargajoe <varga.yosh@gmail.com>
2022-02-11 23:28:40 +0100

    page wrapper naming fix

--- src/components/page-wrapper.js
+++ src/components/page-wrapper.js
@@ -8,10 +8,8 @@ const DATA = require('../config.json');
 export const PageWrapper = (props) => {
   const repo = useRepository();
   const [wrappercompo, setCompo] = useState([]);
-  // const { categoryName } = useParams();
-  const categoryName = props.location.pathname;
   const [context, setContext] = useState();
-  const [pageTemplates, setPts] = useState([]);
+  const locationPath = props.location.pathname;
   console.log("props");
   console.log(props);  
 
@@ -19,26 +17,26 @@ export const PageWrapper = (props) => {
   console.log("path");
   console.log(path);
 
-  function pageQuery () { 
-    let query="";
-    if (context.Type === 'Page' || context.Type === 'Layout') {
-      query = `Path:'${context.Path}' OR TypeIs:Widget AND InTree:'${context.Path}' AND Hidden:0`;
-    } else {
-      query =`((Name:'${context.Type}' OR (Name:'This' AND InFolder:'${context.Path}/(layout)')) AND Type:(Page Layout))
-      OR TypeIs:Widget 
-      AND InTree:(
-        '${context.Path}/(layout)' 
-        '${process.env.REACT_APP_PAGECONTAINER_PATH || DATA.pagecontainerPath}/${context.Type}'       
-        ) 
-      AND Hidden:0`;
-    }
-
-    console.log('query', query);
-    return query;   
-  };
-
   // refactor: filters should get from page fields
   const loadPage = useCallback(async () => {
+    const pageQuery = () => { 
+      let query="";
+      if (context.Type === 'Page' || context.Type === 'Layout') {
+        query = `Path:'${context.Path}' OR TypeIs:Widget AND InTree:'${context.Path}' AND Hidden:0`;
+      } else {
+        query =`((Name:'${context.Type}' OR (Name:'This' AND InFolder:'${context.Path}/(layout)')) AND Type:(Page Layout))
+        OR TypeIs:Widget 
+        AND InTree:(
+          '${context.Path}/(layout)' 
+          '${process.env.REACT_APP_PAGECONTAINER_PATH || DATA.pagecontainerPath}/${context.Type}'       
+          ) 
+        AND Hidden:0`;
+      }
+  
+      console.log('query', query);
+      return query;   
+    };
+
     console.log('context type', context.Type);
     if (context !== undefined && context.Type !== undefined && context.Type !== []) {
       // const query = (context.Type === 'Page' || context.Type === 'Layout') ? 
@@ -59,13 +57,10 @@ export const PageWrapper = (props) => {
         },
       }).then(result => {
         if (result?.d?.results && result?.d?.results.length > 0) {
-          console.log("page", result.d.results);
           const page = result.d.results.sort((a, b) => a.Depth < b.Depth ? 1 : -1).filter(pcnt => pcnt.Type === 'Page' || pcnt.Type === 'Layout')[0];
           const widgets = result.d.results.filter(pcnt => pcnt.ParentId === page.Id);
           const pageTemplate = page.PageTemplate === '' || page.PageTemplate === null ? "vanilla" : page.PageTemplate;
-          console.log('selected page: ', page);
-          console.log('selected widgets', widgets);
-          console.log('selected pagetemplate', pageTemplate);
+          console.log('selected page: ', result.d.results, page, widgets, pageTemplate );
 
           // setCompo(addComponent('layouts', 'page', "vanilla", `cnt-${context.Id}`, context));
           setCompo(addComponent('layouts', 'page', pageTemplate, `page-${context.Id}`, context, page, widgets)); 
@@ -83,9 +78,9 @@ export const PageWrapper = (props) => {
   }, [context, repo]);
 
   const loadContent = useCallback(async () => {
-    const categoryNameWorkaround = categoryName.replace("(", "%28").replace(")", "%29");
+    const locationPathWorkaround = locationPath.replace("(", "%28").replace(")", "%29");
     await repo.load({
-      idOrPath: `${process.env.REACT_APP_DATA_PATH || DATA.dataPath}/${categoryNameWorkaround}`,
+      idOrPath: `${process.env.REACT_APP_DATA_PATH || DATA.dataPath}/${locationPathWorkaround}`,
       oDataOptions: {
         select: 'all',
         expand: 'Workspace'
@@ -98,13 +93,14 @@ export const PageWrapper = (props) => {
       };
     })
     .catch(error => {
-      setCompo(addComponent('content', 'content', 'missing', 1));
+      // setCompo(addComponent('content', 'content', 'missing', 1));
+      setCompo(addComponent('layout', 'page', 'error', 1));
     });
-  }, [categoryName, repo]);
+  }, [locationPath, repo]);
 
   useEffect(() => {
     loadContent();
-  }, [categoryName, loadContent, repo]);
+  }, [locationPath, loadContent, repo]);
 
   useEffect(() => {
     if (context !== undefined && context !== []) {
