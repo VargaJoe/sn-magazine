1e57bffcb5acf5cbd375c26f8c3a10951f470764
vargajoe <varga.yosh@gmail.com>
2022-02-11 19:09:41 +0100

    fix classname in gallery item component

--- src/components/content/component-gallery-item.js
+++ src/components/content/component-gallery-item.js
@@ -23,19 +23,19 @@ export function CustomGalleryItem(props) {
               <div className="context-info">
                   <Link key={`gallery-item-${context.Id}`} to={'/' + relativePath} className="side-menu-link">
                   {/* <svg width="200px" height="200px" > */}
-                    {/* <img src={DATA.apiUrl + context.Path + '/cover.jpg'} alt={context.DisplayName} class="w3-hover-opacity w3-col"/> */}
-                    {/* <img src={DATA.apiUrl + DATA.dataPath + '/(structure)/Site/sample.png'} alt={context.DisplayName} class="w3-hover-opacity w3-col"/> */}
-                    <img src={(process.env.REACT_APP_API_URL || DATA.apiUrl) + context.Image.Url} alt={context.DisplayName} class="w3-hover-opacity w3-col"/>
+                    {/* <img src={DATA.apiUrl + context.Path + '/cover.jpg'} alt={context.DisplayName} className="w3-hover-opacity w3-col"/> */}
+                    {/* <img src={DATA.apiUrl + DATA.dataPath + '/(structure)/Site/sample.png'} alt={context.DisplayName} className="w3-hover-opacity w3-col"/> */}
+                    <img src={(process.env.REACT_APP_API_URL || DATA.apiUrl) + context.Image.Url} alt={context.DisplayName} className="w3-hover-opacity w3-col"/>
                   {/* </svg> */}
-                    <div class="w3-container w3-white list-box-title">
+                    <div className="w3-container w3-white list-box-title">
                         <p>
                           {/* <b>{(context.DisplayName.indexOf('-') > 0)?context.DisplayName.substr(0, context.DisplayName.indexOf('-')):context.DisplayName}</b> */}
                           <b>{context.DisplayName}</b>
-                          {/* <span class="hide-icon"><i class="fa fa-download"></i></span> */}
+                          {/* <span className="hide-icon"><i className="fa fa-download"></i></span> */}
                         </p>
-                        <p class="hidden"></p>
-                        <div class="small hidden">{context.Author}</div>
-                        <div class="small hidden">{Moment(context.PublishDate).format('yyyy.MM.DD')}</div>
+                        <p className="hidden"></p>
+                        <div className="small hidden">{context.Author}</div>
+                        <div className="small hidden">{Moment(context.PublishDate).format('yyyy.MM.DD')}</div>
                     </div>
                   </Link>
                 </div>
