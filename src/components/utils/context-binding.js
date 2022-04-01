82d3172211c1026f2aa083a61396f011e27a4d0a
vargajoe <varga.yosh@gmail.com>
2022-04-01 21:07:24 +0200

    refactor bindedcontext

--- src/components/utils/context-binding.js
+++ src/components/utils/context-binding.js
@@ -16,64 +16,81 @@ export function BindedContext(props, withChildren) {
   const context = props.data;
 
   const loadContents = useCallback(async () => {
-    function getContextPath() {
-      let pathTemp = context.path;
+    function getPreContextObj() {
+      let resultObj = {
+        contextPath: context.Path,
+            content: context,
+            children: [],
+            reload: (withChildren) ? true : false
+      }
       switch (widget.ContextBinding[0]) {
         case "customroot":
           if (widget.CustomRoot !== undefined) {
-            // context could be set here too            
-            pathTemp = widget.CustomRoot.Path;
+            resultObj.contextPath = widget.CustomRoot.Path
+            resultObj.content = widget.CustomRoot
           } else {
             console.log("customroot is not set");
           }
           break;
         case "currentsite":
-          pathTemp = process.env.REACT_APP_DATA_PATH || DATA.dataPath;
+          if (context.Workspace !== null && context.Workspace !== undefined) {
+            resultObj.contextPath = context.Workspace?.Path
+            resultObj.content = context.Workspace
+          } else {
+            resultObj.contextPath = process.env.REACT_APP_DATA_PATH || DATA.dataPath
+            resultObj.content = context.Workspace
+          }
           break;
         default:
           //no problem
       }
   
       if (widget.RelativePath !== null && widget.RelativePath !== undefined) {
-        pathTemp += widget.RelativePath;
+        resultObj.contextPath += widget.RelativePath
+        resultObj.reload = true
       }
-  
-      return pathTemp;
+
+      return resultObj;
     };
 
-    const contextPath = getContextPath();
+
+    // currentcontext, customroot, currentsite(=workspace) gives context immediatelly 
+    // const contextPath = getContextPath();
+    const contextObj = getPreContextObj();
+
+    if (!contextObj.reload) {
+      setContext(contextObj)
+    }
+
     const options = {
       expand: 'Workspace',
       select: 'all',
       metadata: 'no'
     }
     // when context is a smartfolder we must not use query as it would use InTree and smartfolder's own query would be busted
-    if (widget.ContextBinding[0] === 'customroot' && widget.CustomRoot?.Type !== 'SmartFolder') {
-      options.query = '(' + widget.ContentQuery + ` +InFolder:'${contextPath}') Path:'${contextPath}'`
+    if (contextObj.content.Type !== 'SmartFolder') {
+      options.query = '(' + widget.ContentQuery + ` +InFolder:'${contextObj.contextPath}') Path:'${contextObj.contextPath}'`
     } 
 
+    // relativepath have to be loaded
     const result = await repo.loadCollection({
-      path: `${contextPath}`,
+      path: `${contextObj.contextPath}`,
       oDataOptions: options,
     });
     if (result?.d?.results) {
-      setContext({
-        contextPath: contextPath,
-        content: (result.d.results.filter(cnt => cnt.Path === contextPath)[0] !== undefined) 
-          ? result.d.results.filter(cnt => cnt.Path === contextPath)[0] 
-          : context,
-        children: result.d.results      
-          .filter(cnt => cnt.Path !== contextPath)
-      });
+      contextObj.children = result.d.results.filter(cnt => cnt.Path !== contextObj.contextPath)
+
+      if (result.d.results.filter(cnt => cnt.Path === contextObj.contextPath)[0] !== undefined) {
+        contextObj.content = result.d.results.filter(cnt => cnt.Path === contextObj.contextPath)[0] 
+      }
+      setContext(contextObj);
     } else {
     }
-  }, [context, repo, widget]);
+  }, [context, repo, widget.ContentQuery, widget.ContextBinding, widget.CustomRoot, widget.RelativePath, withChildren]);
 
   useEffect(() => {
-    if (withChildren) {
-      loadContents();
-    } 
-  }, [loadContents, withChildren]);
+    loadContents();
+  }, [loadContents]);
   
   console.log('bindedContext result', bndContext);
   return bndContext;
