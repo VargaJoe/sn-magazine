58b293fbefbdcca718b8ec7b79addc8c0938fa27
vargajoe <varga.yosh@gmail.com>
2022-05-04 20:59:11 +0200

    add sample content views

---
+++ src/components/content/auto-workspace.js
@@ -0,0 +1,30 @@
+import ShowDebugInfo from "../utils/show-debuginfo"
+import BindedContext from "../utils/context-binding"
+
+export function GenericContent(props) {
+  const layout = props.page;
+  let context = props.data;
+  const widget = props.widget;
+  const bindedContext = BindedContext(props, true);
+  console.log('default content', context.DisplayName, props, layout, context, widget);
+
+  return (
+    // <div className="w3-col m9 w3-right">
+    <div className="w3-row-padding w3-margin-bottom">
+      <div className="w3-col m12">
+        <div className="w3-card w3-round w3-white">
+        {ShowDebugInfo("default content", context, layout, widget)}
+          <div className="w3-container w3-padding">
+            <h1>{bindedContext.content.DisplayName}</h1>
+            <div className="context-info">
+              <div dangerouslySetInnerHTML={{ __html: context.Description }}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
+}
+
+export default GenericContent;
