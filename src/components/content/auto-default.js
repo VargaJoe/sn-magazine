6f90869eb7444ef30f4a9d429c1d6b7aac1f412e
VargaJoe <varga.yosh@gmail.com>
2022-05-04 21:23:09 +0200

    Feature/demo site (#15)
    * start demo site features
    * add without layout content logic
    * refactor resolve layout logic by ancestor paths
    * add bindedcontext default widget components
    * add default components
    * add sample content views and breadcrumb
    * add sample layout ctd (uncleaned)
    * revert vanilla default logo
    * start of readme

--- src/components/content/auto-default.js
+++ src/components/content/auto-default.js
@@ -1,12 +1,11 @@
 import ShowDebugInfo from "../utils/show-debuginfo"
-
-const DATA = require('../../config.json');
+import BindedContext from "../utils/context-binding"
 
 export function GenericContent(props) {
   const layout = props.page;
   let context = props.data;
   const widget = props.widget;
-
+  const bindedContext = BindedContext(props, true);
   console.log('default content', context.DisplayName, props, layout, context, widget);
 
   return (
@@ -16,10 +15,30 @@ export function GenericContent(props) {
         <div className="w3-card w3-round w3-white">
         {ShowDebugInfo("default content", context, layout, widget)}
           <div className="w3-container w3-padding">
-            <h1>{context.DisplayName}</h1>
-            <div className="context-info">
-              <div dangerouslySetInnerHTML={{ __html: context.Description }}/>
-            </div>
+            <h1>{bindedContext.content.DisplayName}</h1>
+            <table className="w3-table-all">
+              <tbody>
+                <tr>
+                  <th>Field Name</th>
+                  <th>Field Value</th>
+                </tr>
+                {Object.keys(bindedContext.content).map((key, i) => {
+                if (typeof bindedContext.content[key] !== 'object') {
+                return ( 
+                  <tr key={Math.floor(Math.random() * 100000)}>
+                    <td >{key}</td>
+                    <td>{bindedContext.content[key]?.toString()}</td>
+                  </tr>
+                )} else {return (
+                  <tr  key={Math.floor(Math.random() * 100000)} className="hidden">
+                    <td >{key}</td>
+                    <td>{bindedContext.content[key]?.toString()}</td>
+                  </tr>
+                )}
+                }
+                )}
+                </tbody>
+              </table>
           </div>
         </div>
       </div>
