51aed806d745503baf7a6172a9402326750a33e5
vargajoe <varga.yosh@gmail.com>
2022-05-08 20:51:20 +0200

    add component name constant to avoid typo

--- src/components/content/auto-genericcontent.js
+++ src/components/content/auto-genericcontent.js
@@ -1,18 +1,18 @@
 import ShowDebugInfo from "../utils/show-debuginfo"
 
 export function GenericContent(props) {
+  const componentName = 'generic content'
   const layout = props.page;
   let context = props.data;
   const widget = props.widget;
-
-  console.log('generic content', context.DisplayName, props, layout, context, widget);
+  console.log(componentName, context.DisplayName, props, layout, context, widget);
 
   return (
     // <div className="w3-col m9 w3-right">
     <div className="w3-row-padding w3-margin-bottom">
       <div className="w3-col m12">
         <div className="w3-card w3-round w3-white">
-        {ShowDebugInfo("generic content", context, layout, widget)}
+        {ShowDebugInfo(componentName, context, layout, widget)}
           <div className="w3-container w3-padding">
             <h1>{context.DisplayName}</h1>
             <div className="context-info">
--- src/components/content/auto-workspace.js
+++ src/components/content/auto-workspace.js
@@ -1,19 +1,20 @@
 import ShowDebugInfo from "../utils/show-debuginfo"
 import BindedContext from "../utils/context-binding"
 
-export function GenericContent(props) {
+export function Workspace(props) {
+  const componentName = 'workspace'
   const layout = props.page;
   let context = props.data;
   const widget = props.widget;
   const bindedContext = BindedContext(props, true);
-  console.log('default content', context.DisplayName, props, layout, context, widget);
-
+  console.log(componentName, context.DisplayName, props, layout, context, widget);
+  
   return (
     // <div className="w3-col m9 w3-right">
     <div className="w3-row-padding w3-margin-bottom">
       <div className="w3-col m12">
         <div className="w3-card w3-round w3-white">
-        {ShowDebugInfo("default content", context, layout, widget)}
+        {ShowDebugInfo(componentName, context, layout, widget)}
           <div className="w3-container w3-padding">
             <h1>{bindedContext.content.DisplayName}</h1>
             <div className="context-info">
@@ -27,4 +28,4 @@ export function GenericContent(props) {
   );
 }
 
-export default GenericContent;
+export default Workspace;
