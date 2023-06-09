6569eca5b0ca2422e9bd01f5353ad8b06c8946b1
vargajoe <varga.yosh@gmail.com>
2023-06-09 22:36:14 +0200

    update helmet logic

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import "./App.css";
 import "./App-Custom.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SiteRoutes from "./navigation";
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
+  const helmetContext = {};
+
   return (
-    <BrowserRouter>
-     <Routes>
-        {SiteRoutes.public.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            element={<route.component/>}
-          />
-        ))}
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider context={helmetContext}>
+      <BrowserRouter>
+        <Routes>
+          {SiteRoutes.public.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              element={<route.component />} />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
