8d5a91d47fe16553555c7af442d69a29ea22bc2f
vargajoe <varga.yosh@gmail.com>
2023-06-09 21:46:14 +0200

    update router logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 import "./App-Custom.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SiteRoutes from "./navigation";
 
 function App() {
   return (
     <BrowserRouter>
-     <Switch>
+     <Routes>
         {SiteRoutes.public.map((route, index) => (
           <Route
             key={index}
             path={route.path}
             exact={route.exact}
-            component={route.component}
+            element={<route.component/>}
           />
         ))}
-      </Switch>
+      </Routes>
     </BrowserRouter>
   );
 }
