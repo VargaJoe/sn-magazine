2878faf8748ff6546e49222c74dd81823fd44895
vargajoe <varga.yosh@gmail.com>
2023-06-10 20:54:02 +0200

    add login widget base

---
+++ src/components/widgets/auto-widgetlogin.js
@@ -0,0 +1,30 @@
+import {
+  useOidcAuthentication,
+} from "@sensenet/authentication-oidc-react";
+
+import ShowDebugInfo from "../utils/show-debuginfo"
+export function LoginWidget(props) {
+  console.log('%clogin component', "font-size:16px;color:green");
+  console.log(props);
+  const layout = props.page;
+  const context = props.data;
+  const widget = props.widget;
+
+  const { oidcUser, login, logout } = useOidcAuthentication();
+  const button = oidcUser ? <button onClick={logout}>Logout</button> : <button onClick={login}>Login</button>;
+
+  return (
+    <div className="w3-card w3-round w3-white w3-margin-bottom">
+    {ShowDebugInfo("side menu with logo", context, layout, widget)}
+    <div className="w3-container">
+      <div className="side-menu-uppercase">
+        <p>
+          {button}
+        </p>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default LoginWidget
