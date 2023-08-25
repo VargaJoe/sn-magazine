5e1c144c50682f71e663a8343a24bf668f8530c4
vargajoe <varga.yosh@gmail.com>
2023-08-25 21:53:28 +0200

    test browserrouter - helmetprovider switch

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import "./App.css";
 import "./App-Custom.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SiteRoutes from "./navigation";
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   const helmetContext = {};
 
   return (
-    <HelmetProvider context={helmetContext}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <HelmetProvider context={helmetContext}>
         <Routes>
           {SiteRoutes.public.map((route, index) => (
             <Route
               key={index}
               path={route.path}
               exact={route.exact}
-              element={<route.component />} />
+              element={<route.component />}
+            />
           ))}
         </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+      </HelmetProvider>
+    </BrowserRouter>
   );
 }
 
