3047673446a46aa1e996ad8d8e4c72d2f0b61b10
vargajoe <varga.yosh@gmail.com>
2023-12-18 11:29:09 +0100

    cleanup

--- src/components/layouts/page-explore.js
+++ src/components/layouts/page-explore.js
@@ -37,13 +37,6 @@ export const ExploreLayout = (props) => {
           >
             <i className="fa fa-globe"></i>
           </a>
-          {/* <a
-            href="/"
-            className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"
-            title="Account Settings"
-          >
-            <i className="fa fa-user"></i>
-          </a> */}
           <a
            href="mailto:info@sensenet.com"
            className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"
@@ -51,37 +44,6 @@ export const ExploreLayout = (props) => {
          >
            <i className="fa fa-envelope"></i>
          </a>
-          {/* <div className="w3-dropdown-hover w3-hide-small">
-            <button
-              className="w3-button w3-padding-large"
-              title="Notifications"
-            >
-              <i className="fa fa-bell"></i>
-              <span className="w3-badge w3-right w3-small w3-green">3</span>
-            </button>
-            <div className="w3-dropdown-content w3-card-4 w3-bar-block w3-dropdown-content-custom">
-              <a href="/" className="w3-bar-item w3-button">
-                One new friend request
-              </a>
-              <a href="/" className="w3-bar-item w3-button">
-                John Doe posted on your wall
-              </a>
-              <a href="/" className="w3-bar-item w3-button">
-                Jane likes your post
-              </a>
-            </div>
-          </div> */}
-          {/* <a
-            href="/"
-            className="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white"
-            title="My Account"
-          >
-            <img
-              src="/w3images/avatar2.png"
-              className="w3-circle w2-circle-custom"
-              alt="Avatar"
-            />
-          </a> */}
         </div>
       </div>
 
@@ -89,10 +51,6 @@ export const ExploreLayout = (props) => {
         id="navDemo"
         className="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large"
       >
-        {/* <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 1</a>
-  <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 2</a>
-  <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 3</a>
-  <a href="#" className="w3-bar-item w3-button w3-padding-large">My Profile</a> */}
       </div>
       
       {/* Page Container */}
@@ -131,12 +89,6 @@ export const ExploreLayout = (props) => {
           </div>
           {/* End Middle Column */}
 
-          {/* Right Column */}
-          {/* <div className="w3-col m2">
-            <SideReviews />
-            <SideTranslations />
-          </div> */}
-          {/* End Right Column */}
         </div>
         {/* End Grid */}
       </div>
--- src/components/layouts/page-vanilla.js
+++ src/components/layouts/page-vanilla.js
@@ -36,13 +36,6 @@ export const VanillaPageTemplate = (props) => {
           >
             <i className="fa fa-globe"></i>
           </a>
-          {/* <a
-            href="/"
-            className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"
-            title="Account Settings"
-          >
-            <i className="fa fa-user"></i>
-          </a> */}
             <a
            href="mailto:info@sensenet.com"
            className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white"
@@ -50,37 +43,6 @@ export const VanillaPageTemplate = (props) => {
          >
            <i className="fa fa-envelope"></i>
          </a>
-          {/* <div className="w3-dropdown-hover w3-hide-small">
-            <button
-              className="w3-button w3-padding-large"
-              title="Notifications"
-            >
-              <i className="fa fa-bell"></i>
-              <span className="w3-badge w3-right w3-small w3-green">3</span>
-            </button>
-            <div className="w3-dropdown-content w3-card-4 w3-bar-block w3-dropdown-content-custom">
-              <a href="/" className="w3-bar-item w3-button">
-                One new friend request
-              </a>
-              <a href="/" className="w3-bar-item w3-button">
-                John Doe posted on your wall
-              </a>
-              <a href="/" className="w3-bar-item w3-button">
-                Jane likes your post
-              </a>
-            </div>
-          </div> */}
-          {/* <a
-            href="/"
-            className="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white"
-            title="My Account"
-          >
-            <img
-              src="/w3images/avatar2.png"
-              className="w3-circle w2-circle-custom"
-              alt="Avatar"
-            />
-          </a> */}
         </div>
       </div>
 
@@ -88,10 +50,6 @@ export const VanillaPageTemplate = (props) => {
         id="navDemo"
         className="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large"
       >
-        {/* <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 1</a>
-  <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 2</a>
-  <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 3</a>
-  <a href="#" className="w3-bar-item w3-button w3-padding-large">My Profile</a> */}
       </div>
       
       {/* Page Container */}
@@ -121,13 +79,6 @@ export const VanillaPageTemplate = (props) => {
             {components}
           </div>
           {/* End Middle Column */}
-
-          {/* Right Column */}
-          {/* <div className="w3-col m2">
-            <SideReviews />
-            <SideTranslations />
-          </div> */}
-          {/* End Right Column */}
         </div>
         {/* End Grid */}
       </div>
