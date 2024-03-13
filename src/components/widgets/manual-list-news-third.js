fd931d8e8c24766f43f05e3c8a43ac2f81e6830c
vargajoe <varga.yosh@gmail.com>
2024-03-13 23:03:31 +0100

    update thirdsized news with store

--- src/components/widgets/manual-list-news-third.js
+++ src/components/widgets/manual-list-news-third.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { addComponent } from '../utils/add-component';
 import ShowDebugInfo from "../utils/show-debuginfo"
 import BindedContext from "../utils/context-binding"
+import { useSnStore } from "../store/sn-store";
 
-export function HalfSizedNewsList(props) {
-  console.groupCollapsed('%chalfSizedNewsList', "font-size:16px;color:green");
-  console.log('props', props);
-  const layout = props.page;
-  let context = props.data;
+
+export function ThirdSizedNewsList(props) {
+  console.log('%cThirdSizedNewsList', "font-size:16px;color:green", { props: props });
+  // const layout = props.page;
+  // let context = props.data;
+  const {context, page, layout} = useSnStore((state) => state);
   let widget = props.widget;
   const bindedContext = BindedContext(props, true);
 
@@ -15,7 +17,7 @@ export function HalfSizedNewsList(props) {
     // <div className="w3-col m9 w3-right">
         <div className="w3-margin-bottom w3-col m4 news-padding">
           <div className="w3-card w3-round w3-white">
-            {ShowDebugInfo("half size news widget", context, layout, widget)}
+            {ShowDebugInfo("half size news widget", context, page, widget, layout)}
             <div className="w3-container w3-padding component-news-half">
             <h3>{bindedContext?.content?.DisplayName}</h3>
               <div>
@@ -25,10 +27,9 @@ export function HalfSizedNewsList(props) {
               </div>
             </div>
           </div>
-          {console.groupEnd()}
         </div>
     // </div>
   );
 }
 
-export default HalfSizedNewsList
+export default ThirdSizedNewsList
