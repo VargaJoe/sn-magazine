e49ac88b4549d876e2dda286e1f4e8a4fabf4d0a
vargajoe <varga.yosh@gmail.com>
2024-03-16 21:56:06 +0100

    tranlation list optimization - p2

--- src/components/widgets/manual-list-translations.js
+++ src/components/widgets/manual-list-translations.js
@@ -4,14 +4,14 @@ import { useRepository } from "@sensenet/hooks-react";
 import LazyLoad from 'react-lazyload';
 import ShowDebugInfo from "../utils/show-debuginfo"
 import BindedContext from "../utils/context-binding"
+import { useSnStore } from "../store/sn-store";
 
-export function NewsListComponent(props) {
+export function TranslationListWidget(props) {
   //console.log('props', props);
   const repo = useRepository();
   const [itemCollection, setCollection] = useState([]);
-  const layout = props.page;
-  let context = props.data;
-  let widget = props.widget;
+  const {context, page, layout} = useSnStore((state) => state);
+  const widget = props.widget;
   
   // binded context could be result back with all widget variables contet, children, widget, layout
   // binded context withChildren should be set on widget content, not manual boolean variable
@@ -23,43 +23,44 @@ export function NewsListComponent(props) {
       path: `/Root/Content/mangajanlo/manga`,
       oDataOptions: {
         query: `Translation:(${ids})`,
-        select: 'all'
+        select: 'all',
+        expand: 'Translation',
       },
     });
     if (result?.d?.results) {
-      //console.log('mangas with translation: ', result);
       setCollection(result.d.results);
     }
-  }, [context, repo]);
+  }, [bindedContext.children, repo]);
 
   useEffect(() => {
-    if (bindedContext.children?.count > 0) {
+    if (bindedContext.children?.length > 0) {
       loadContents();
     }
-  }, [bindedContext.children?.count, context, loadContents, repo]);
+  }, [bindedContext.children.count, bindedContext.children?.length, context, loadContents, repo]);
 
 
   return (
-    // <div className="w3-col m12 w3-right">
-        <div className="w3-margin-bottom w3-col m12 news-padding">
-          <div className="w3-card w3-round w3-white">
-          {ShowDebugInfo("news widget", context, layout, widget)}
-            <div className="w3-container w3-padding component-news-half">
-            <h3>{bindedContext?.content?.DisplayName}</h3>
-              <div>
-                {bindedContext.children?.map((child) => {
-                  return (
-                    <LazyLoad key={`news-list-${child?.Id}`} className="lazy-load-news-item">
-                      {addComponent('widgets', 'nested', `list-translations-item`, `${widget?.Id}-${context?.Id}-${child?.Id}`, child, props?.page, child)}
-                    </LazyLoad>
-                  )
-                })}
-              </div>
-            </div>
+    <div className="w3-margin-bottom w3-col m12 news-padding">
+      <div className="w3-card w3-round w3-white">
+      {ShowDebugInfo("translations widget", context, page, widget, layout)}
+        <div className="w3-container w3-padding component-news-half">
+        <h3>{bindedContext?.content?.DisplayName}</h3>
+          <div className="news-cards">
+            {bindedContext.children?.map((child) => {
+              let filteredItems = itemCollection.filter(item => 
+                item.Translation.some(subItem => child.Id === subItem.Id)
+              );
+              return (
+                <LazyLoad key={`news-list-${child?.Id}`} className="lazy-load-news-item">
+                  {addComponent('widgets', 'nested', `list-translations-item`, `${widget?.Id}-${context?.Id}-${child?.Id}`, {context: child, itemCollection: filteredItems }, props?.page, child)}
+                </LazyLoad>
+              )
+            })}
           </div>
         </div>
-    // </div>
+      </div>
+    </div>
   );
 }
 
-export default NewsListComponent
+export default TranslationListWidget
--- src/components/widgets/nested-list-translations-item.js
+++ src/components/widgets/nested-list-translations-item.js
@@ -7,37 +7,16 @@ import { useSnStore } from "../store/sn-store";
   
 export function NestedLeisureMangaTranslationItem(props) {
   const repo = useRepository();
-  // const [itemCollection, setCollection] = useState([]);
-  let context = props.data;
-  let itemCollection = props.related;
+  let { context, itemCollection } = props.data;
   const {page, layout} = useSnStore((state) => state);
   const widget = props.widget;
-  //console.log('nestedLeisureMangaTranslationItem', props, context);
+  console.log('%cNestedLeisureMangaTranslationItem',  "font-size:16px;color:green", {props: props});
   
-  // const loadContents = useCallback(async () => {
-  //   const result = await repo.loadCollection({
-  //     path: `/Root/Content/mangajanlo/manga`,
-  //     oDataOptions: {
-  //       query: `Translation:(${context.Id})`,
-  //       select: 'all'
-  //     },
-  //   });
-  //   if (result?.d?.results) {
-  //     //console.log('manga with translation: ', result);
-  //     setCollection(result.d.results);
-  //   }
-  // }, [context, repo]);
-
-  // useEffect(() => {
-  //   loadContents();
-  // }, [context, loadContents, repo]);
-
   return (
-    // <div className="w3-col m9 w3-right">
     <div className="w3-row-padding w3-margin-bottom w3-left w3-block">
       <div className="w3-col">
         <div className="w3-card w3-round w3-white">
-        {ShowDebugInfo("leisure manga translation news item", context, layout, widget)}
+        {ShowDebugInfo("leisure manga translation news item", context, page, widget, layout)}
           <div className="w3-container w3-padding">
             <div className="w3-padding-16 w3-clear">
                 {/* <Link key={`news-item-${context.Id}`} to={'/' + relativePath} className="no-score"> */}
@@ -60,7 +39,6 @@ export function NestedLeisureMangaTranslationItem(props) {
         </div>
       </div>
     </div>
-    // </div>
   );
 }
 
