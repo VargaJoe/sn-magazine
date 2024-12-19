73aad867092a578bf5de90ea15910f9e1c496837
vargajoe <varga.yosh@gmail.com>
2024-12-19 23:09:54 +0100

    bind facebook plugin to widget context

--- src/components/widgets/manual-plugin-fbcomments.js
+++ src/components/widgets/manual-plugin-fbcomments.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import ShowDebugInfo from "../utils/show-debuginfo"
-// import BindedContext from "../utils/context-binding"
+import BindedContext from "../utils/context-binding"
 import { useSnStore } from "../store/sn-store";
 const DATA = require('../../config.json');
 
@@ -10,17 +10,17 @@ export function FacebookCommentsWidget(props) {
   // let context = props.data;
   const {context, page, layout} = useSnStore((state) => state);
   let widget = props.widget;  
-  // const bindedContext = BindedContext(props, true);
+  const bindedContext = BindedContext(props, true);
   const fbCommentsRef = useRef(null);
   const [width, setWidth] = useState('');
 
   const appId = process.env.REACT_APP_FB_APPID || DATA.facebookappid;
   const siteHost = process.env.REACT_APP_SITE_HOST || DATA.siteHost;
   const dataPath = process.env.REACT_APP_DATA_PATH || DATA.dataPath;
-  const pagePath = context.Path.replace(dataPath, '');
+  // const pagePath = context.Path.replace(dataPath, ''); // url context content path
+  const pagePath = bindedContext.contextPath.replace(dataPath, ''); // widget context content path
   const pageUrl = siteHost + pagePath;
-  console.log('componentsize', fbCommentsRef?.current?.offsetWidth);
-
+  
   useEffect(() => {
     const loadFacebookSDK = () => {
       if (document.getElementById('facebook-jssdk')) {
