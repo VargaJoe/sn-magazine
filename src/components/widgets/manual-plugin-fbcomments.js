77c3ad3f2e031e2b7f1d796db2049d18fcfdce60
VargaJoe <17780970+VargaJoe@users.noreply.github.com>
2024-12-20 20:47:23 +0100

    Feature/Facebook Comments Widget (#59)
    * add debug log for lazy importview
    
    * POC facebook plugin widget (w resize problem)
    
    * bind facebook plugin to widget context
    
    * add cleanup gitignore
    
    * auto width of comments
    
    * some tweaks to comment plugin
    
    * check appid
    
    * fix style

---
+++ src/components/widgets/manual-plugin-fbcomments.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+import ShowDebugInfo from "../utils/show-debuginfo"
+import BindedContext from "../utils/context-binding"
+import { useSnStore } from "../store/sn-store";
+const DATA = require('../../config.json');
+
+// Facebook Comments Plugin
+// https://developers.facebook.com/docs/plugins/comments
+
+export function FacebookCommentsWidget(props) {
+  console.log('%cFbComments', "font-size:16px;color:green", { props: props });
+  // const layout = props.page;
+  // let context = props.data;
+  const {context, page, layout} = useSnStore((state) => state);
+  let widget = props.widget;  
+  const bindedContext = BindedContext(props, true);
+
+  const appId = process.env.REACT_APP_FB_APPID || DATA.facebookappid;
+  const siteHost = process.env.REACT_APP_SITE_HOST || DATA.siteHost;
+  const dataPath = process.env.REACT_APP_DATA_PATH || DATA.dataPath;
+  // const pagePath = context.Path.replace(dataPath, ''); // url context content path
+  const pagePath = bindedContext.contextPath.replace(dataPath, ''); // widget context content path
+  const pageUrl = siteHost + pagePath;
+  
+  useEffect(() => {
+    const loadFacebookSDK = () => {
+      if (document.getElementById('facebook-jssdk') || !appId) {
+        return;
+      }
+      const script = document.createElement('script');
+      script.id = 'facebook-jssdk';
+      script.src = `https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v21.0&appId=${appId}`;
+      script.async = true;
+      script.defer = true;
+      script.crossOrigin = 'anonymous';
+      document.body.appendChild(script);
+    };
+
+    loadFacebookSDK();
+    
+    if (window.FB && window.FB.XFBML && typeof window.FB.XFBML.parse === 'function') {
+      window.FB.XFBML.parse();
+    }
+  }, [appId]);
+
+  return (
+    <div className="w3-row-padding w3-margin-bottom full-width">
+      <div className="w3-col m12">
+        <div className="w3-card w3-round w3-white">
+          {ShowDebugInfo("facebook comment", context, page, widget, layout)}
+          <div className="w3-container w3-padding">
+            <div className="fb-comments w3-container w3-padding">
+              <h3>Hozzászólások</h3>
+              <div className="fb-comments" data-href={pageUrl} data-width="100%" data-numposts="5" data-order-by="reverse_time" data-lazy></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FacebookCommentsWidget;
