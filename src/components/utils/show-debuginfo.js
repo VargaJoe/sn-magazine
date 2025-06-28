375404684e54803dc44612e2097c5980db5f063b
VargaJoe <varga.yosh@gmail.com>
2025-06-28 10:15:33 +0200

    feat(dev): persist debug info visibility using localStorage and URL param (Story 01)

--- src/components/utils/show-debuginfo.js
+++ src/components/utils/show-debuginfo.js
@@ -2,21 +2,31 @@ import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { clearLazyComponentCache } from './add-component';
 
+const DEBUG_KEY = 'sn-debug-enabled';
+
 const ShowDebugInfo = (title, context, currentPage, widget, layout) => {
   const [showDebug, setDebug] = useState(false);
+  const location = useLocation();
+
+  // Persist debug mode in localStorage based on URL param
+  React.useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const debugParam = params.get('debug');
+    if (debugParam === 'true') {
+      localStorage.setItem(DEBUG_KEY, 'true');
+    } else if (debugParam === 'false') {
+      localStorage.removeItem(DEBUG_KEY);
+    }
+  }, [location.search]);
+
+  const isDebug =
+    (process.env.NODE_ENV === 'development' && localStorage.getItem(DEBUG_KEY) === 'true');
+
+  if (!isDebug) return;
+
   const handleToggle = () => {
     setDebug(!showDebug);
   };
-
-  function useQuery() {
-    const { search } = useLocation();
-
-    return React.useMemo(() => new URLSearchParams(search), [search]);
-  }
-  const query = useQuery();
-  const isDebug = query.get("debug");
-
-  if (!isDebug || isDebug !== "true") return;
 
   function showContextInfo(context) {
     if (context !== undefined) {
